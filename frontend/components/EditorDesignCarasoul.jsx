576dcd0ca33e6fd7082469b7c0df058b0a07b2f9
Abhishek <21bcs116@nith.ac.in>
2023-12-28 22:17:36 +0530

    color Selection logic Part done :white_check_mark:

--- frontend/components/EditorDesignCarasoul.jsx
+++ frontend/components/EditorDesignCarasoul.jsx
@@ -2,24 +2,49 @@ import React, { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from "react-responsive-carousel";
-import { Switch } from "@nextui-org/react";
 import ImageEditor from "@/components/Editor/index2";
 import Image from "next/image";
+import { Switch } from "antd";
+
+const colors = ["red", "blue", "gray", "yellow"];
+
+const ColorSelection = () => {
+  const [selectedColors, setSelectedColors] = useState([]);
+  console.log(selectedColors);
+  const handleColorChange = (value) => {
+    console.log("this is my value", value);
+    const isColorSelected = selectedColors.includes(value);
+
+    if (!isColorSelected) {
+      setSelectedColors((prevSelectedColors) => prevSelectedColors.push(value));
+    } 
+  };
+
+  return (
+    <div className="flex items-center space-x-4">
+      {colors.map((color) => (
+        <div
+          key={color}
+          className={`w-8 h-8 cursor-pointer rounded-full bg-${color}-500`}
+          onClick={() => handleColorChange(color)}
+        ></div>
+      ))}
+    </div>
+  );
+};
 
 const EditorDesignCarasoul = () => {
   const [isSelected, setIsSelected] = useState(true);
   const [selectedMainImage, setSelectedMainImage] = useState("");
   const [images, setImages] = useState([]);
-  const [backImage, setBackImage] = useState(
-    images.length > 0 ? images[0] : ""
-  );
-  console.log(backImage);
+  const [backImage, setBackImage] = useState("");
+  console.log("this is ", backImage);
   console.log(typeof images[0]);
   const router = useRouter();
   const overlayImageSrc = router.query.url || "";
 
   // handle funcitons
-  const handleImageClick= (img) => {
+  const handleImageClick = (img) => {
     setBackImage(img);
   };
 
@@ -53,6 +78,8 @@ const EditorDesignCarasoul = () => {
     };
 
     fetchData();
+
+    setBackImage(images[0]);
   }, []);
 
   const handleImageSelect = (imageSrc) => {
@@ -60,30 +87,43 @@ const EditorDesignCarasoul = () => {
   };
 
   return (
-    <div className=" text-white text-[20px] w-full max-w-[1360px] mx-auto sticky top-[50px]">
-      <div className="absolute right-1 top-3 z-40 gap-2 flex mr-4 items-center border-2 border-black p-1 px-2 rounded-full bg-zinc-300">
-        <span className="text-black text-sm">Front</span>
-        <Switch
-          className=""
-          isSelected={isSelected}
-          onValueChange={setIsSelected}
-        ></Switch>
-        <span className="text-black text-sm ml-[-7px]">Back</span>
+    <>
+      <div className=" flex gap-4 text-white text-[20px] w-full max-w-[1360px] mx-auto sticky top-[50px]">
+        <div className="absolute right-[80px] top-2 z-40 gap-2 flex mr-4 items-center border-2 border-black p-1 px-2 rounded-full bg-zinc-700">
+          <Switch
+            checkedChildren="Back"
+            unCheckedChildren="Front"
+            defaultChecked
+          />
+        </div>
+
+        <div className="flex">
+          <div className="">
+            {backImage && (
+              <ImageEditor
+                mainImageSrc={backImage}
+                overlayImageSrc={overlayImageSrc}
+              />
+            )}
+          </div>
+        </div>
+        <div className="flex flex-col  gap-2 ">
+          {images.map((imageUrl, index) => (
+            <div
+              className="cursor-pointer border-2 border-black rounded-md"
+              key={index}
+              onClick={() => handleImageClick(imageUrl)}
+            >
+              <Image src={imageUrl} alt="mockup" width={50} height={50}></Image>
+            </div>
+          ))}
+        </div>
       </div>
 
-      {images.map((imageUrl, index) => (
-        <div key={index} onClick={() => handleImageClick(imageUrl)}>
-          <Image src={imageUrl} alt="mockup" width={200} height={200}></Image>
-        </div>
-      ))}
-
-      {backImage && (
-        <ImageEditor
-        mainImageSrc={backImage}
-        overlayImageSrc={overlayImageSrc}
-      /> 
-      )}
-    </div>
+      <div>
+        <ColorSelection />
+      </div>
+    </>
   );
 };
 
