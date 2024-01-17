561a9928af24677a8a4ce1d92e8aa419ac9f0eb6
deauth <anurage66@gmail.com>
2024-01-17 13:04:20 +0530

    style:changed product card component

--- frontend/components/ProductCard.jsx
+++ frontend/components/ProductCard.jsx
@@ -2,47 +2,50 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import { FiShare2 } from "react-icons/fi";
+import { FcLike } from "react-icons/fc";
 import { AiOutlineHeart } from "react-icons/ai";
 const ProductCard = () => {
-  return (
-    <Link
-      href="/"
-      className="transform overflow-hidden bg-white/[0.85] duration-200 hover:scale-[1.02] cursor-pointer rounded-[20px] shadow-md"
-    >
-      <div className="relative">
-        <div className="absolute right-4 top-3 ">
-         <div className="flex flex-col gap-3">
-         <span className="text-xl p-1.5 rounded-full shadowbg-blue-400 transition-all  bg-blue-400 duration-200">
-            {" "}
-            <FiShare2 />
-          </span>
+	return (
+		<Link
+			href="/"
+			className="transform overflow-hidden shadow-md duration-200 hover:scale-[1.02] cursor-pointer rounded-[20px] bg-red-200 w-3/4 border-zinc-600 border-3 bg-white/[0.5] hover:bg-white/[0.85]"
+		>
+			<div className=" ">
+				<div className="mt-2 mx-2 rounded-lg bg-[#bdcd5b] flex-row flex justify-center ">
+					<Image width={300} height={300} src="/shirt.png" alt="shirt" />
+					<div className="flex flex-col md:flex-col absolute bottom-48 right-4 justify-center p-1">
+						<div className="border-2 border-black bg-white/[0.8]  rounded-full w-10 h-10 mr-2"></div>
+					</div>
+				</div>
+			</div>
 
-          <span className="text-xl p-1.5 rounded-full shadow bg-red-400 transition-all duration-200">
-            {" "}
-            <AiOutlineHeart />
-          </span>
-         </div>
-        </div>
-      </div>
-      <Image width={500} height={500} src="/shirt.png" alt="shirt" />
+			<div className="p-4  text-black/[0.9] shadow-md">
+				<h3 className="text-sm font-medium">T-Shirt</h3>
+				<h2 className="text-lg font-extrabold">Game Over Black Men T-Shirt</h2>
+				<h2 className="text-xs font-light">designed by jatt</h2>
+				<div className="my-2 flex flex-row justify-start">
+					<div className="border-2 border-black bg-red-600 rounded-full w-10 h-10 mr-2"></div>
+					<div className="border-2 border-black bg-blue-400 rounded-full w-10 h-10 mr-2"></div>
+					<div className="border-2 border-black bg-yellow-300 rounded-full w-10 h-10 mr-2"></div>
+					<div className="border-2 border-black bg-white rounded-full w-10 h-10 mr-2"></div>
+				</div>
+				<div className="flex items-center text-black/[0.5]">
+					<p className="mr-2 text-lg font-semibold bg-[#bdcd5b] px-1 ">
+						&#8377; 1234
+					</p>
 
-      <div className="p-4 text-black/[0.9]">
-        <h2 className="text-lg font-medium">Game Over Black Men T-Shirt</h2>
-        <div className="flex items-center text-black/[0.5]">
-          <p className="mr-2 text-lg font-semibold">&#8377; 1234</p>
-
-          {true && (
-            <>
-              <p className="text-base  font-medium line-through">&#8377;3468</p>
-              <p className="ml-auto text-base font-medium text-green-500">
-                45 % off
-              </p>
-            </>
-          )}
-        </div>
-      </div>
-    </Link>
-  );
+					{true && (
+						<>
+							<p className="text-base  font-medium line-through">&#8377;3468</p>
+							<button className="bg-transparent border-zinc-600 border-2 text-base ml-auto font-bold text-black rounded-md py-1 px-2">
+								add to cart
+							</button>
+						</>
+					)}
+				</div>
+			</div>
+		</Link>
+	);
 };
 
 export default ProductCard;
