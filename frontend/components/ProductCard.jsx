12e0be33da1a7ec07d2656dc482a62ba1f8e38e3
deauth <anurage66@gmail.com>
2024-01-24 22:15:50 +0530

    added designerDashBoard

--- frontend/components/ProductCard.jsx
+++ frontend/components/ProductCard.jsx
@@ -6,12 +6,17 @@ import { FcLike } from "react-icons/fc";
 import { AiOutlineHeart } from "react-icons/ai";
 //TODO: tshirt animations vertical, pnHover: show backside image
 //later on TODO: add humanmodels in conjunction with clothes on hover
+
 const ProductCard = ({
+	card_type,
 	product_photo,
 	product_colors,
 	product_price,
 	product_stock,
 }) => {
+	if (card_type === "dashboard") {
+		return <DashboardProductCard data="" />;
+	}
 	return (
 		<Link
 			href="/"
@@ -63,6 +68,60 @@ const ProductCard = ({
 						</>
 					)}
 				</div>
+			</div>
+		</Link>
+	);
+};
+
+const DashboardProductCard = ({
+	data,
+	product_photo,
+	product_colors,
+	product_price,
+	product_stock,
+}) => {
+	return (
+		<Link
+			href="/"
+			className="transform overflow-hidden  shadow-lg border border-black/10 md duration-200  cursor-pointer rounded-[20px] "
+		>
+			<div className="h-[350px]">
+				<div className="mt-2 mx-2 rounded-lg flex-row flex justify-center ">
+					{product_photo ? (
+						<Image
+							width={250}
+							height={250}
+							src={product_photo}
+							alt="shirt"
+							className="object-cover"
+						/>
+					) : (
+						<Image width={300} height={300} src="/shirt.png" alt="shirt" />
+					)}
+				</div>
+			</div>
+
+			<div className="p-2 -mt-5 px-3 shadow-md grid grid-cols-3">
+				<div className="text-xs text-black/60 text-wrap mx-auto">
+					<div className="text-md mx-auto text-center">sales</div>
+					<div className="text-xl mx-auto text-center">
+						{data.sales || "$400"}
+					</div>
+				</div>
+				<div className="text-xs text-black/60 text-wrap mx-auto">
+					<div className="text-md mx-auto text-center">revenue</div>
+					<div className="text-xl mx-auto text-center">
+						{data.sales || "$200"}
+					</div>
+				</div>
+				<div className="text-xs text-black/60 text-wrap mx-auto">
+					<div className="text-md mx-auto text-center">profit</div>
+					<div className="text-xl mx-auto text-center">
+						{data.sales || "$100"}
+					</div>
+				</div>
+				<div className="my-2 flex flex-row justify-start"></div>
+				<div className="flex items-center text-black/[0.5] "></div>
 			</div>
 		</Link>
 	);
