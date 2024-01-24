c3d15c0c55d2da24719367852e2c34fc8457343c
deauth <anurage66@gmail.com>
2024-01-24 23:29:35 +0530

    fix: HeroBanner.js

--- frontend/components/HeroBanner.jsx
+++ frontend/components/HeroBanner.jsx
@@ -10,13 +10,9 @@ import { EffectCoverflow, Pagination, Navigation } from "swiper/modules";
 import { TypeAnimation } from "react-type-animation";
 
 const CustomArrow = ({ onClick, icon: Icon, direction }) => {
-
-  const designIt = "Design it";
-  const wearIt = "Wear it";
-  const lovieIt = "Love it";
-  const [isClicked, setIsClicked] = useState(false);
-
-
+	const designIt = "Design it";
+	const wearIt = "Wear it";
+	const lovieIt = "Love it";
 	const [isClicked, setIsClicked] = useState(false);
 
 	const handleClick = () => {
@@ -38,102 +34,100 @@ const CustomArrow = ({ onClick, icon: Icon, direction }) => {
 };
 
 const HeroBanner = () => {
+	const settings = {
+		dots: true,
+		infinite: true,
+		speed: 500,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+	};
 
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-  };
+	return (
+		<div className="m-auto relative mt-9 ">
+			<div className="h-[40px] w-full"></div>
+			<div className="flex justify-center mt-[70px] flex-col text-center gap-3 ">
+				{/* <p className="text-7xl font-bold" ref={element}>Design it, Wear it, Love it</p>
+				 */}
+				{/* <div className="text-7xl font-bold" id="good"></div> */}
 
-  return (
-    <div className="m-auto relative mt-9 ">
-      <div className="h-[40px] w-full"></div>
-      <div className="flex justify-center mt-[70px] flex-col text-center gap-3 ">
-        {/* <p className="text-7xl font-bold" ref={element}>Design it, Wear it, Love it</p>
-         */}
-        {/* <div className="text-7xl font-bold" id="good"></div> */}
+				<div className="text-4xl font-bold ">
+					<TypeAnimation
+						preRenderFirstString={true}
+						sequence={[
+							500,
+							"Design it", // initially rendered starting point
+							1000,
+							"Design it , Wear it",
+							1000,
+							"Design it , Wear it , Love it",
+						]}
+						speed={50}
+						style={{ fontSize: "2em" }}
+						repeat={1}
+						cursor={false}
+					/>
+				</div>
+				<p className="text-4xl mt-3 text-[#595957]">
+					Your Signature Style Awaits
+				</p>
+			</div>
 
-        <div className="text-4xl font-bold ">
-          <TypeAnimation
-            preRenderFirstString={true}
-            sequence={[
-              500,
-              "Design it", // initially rendered starting point
-              1000,
-              "Design it , Wear it",
-              1000,
-              "Design it , Wear it , Love it",
-             
-            ]}
-            speed={50}
-            style={{ fontSize: "2em" }}
-            repeat={1}
-            cursor={false}
-          />
-        </div>
-        <p className="text-4xl mt-3 text-[#595957]">
-          Your Signature Style Awaits
-        </p>
-      </div>
+			<div className="flex justify-center mt-7  ">
+				<button className="flex items-center gap-2 border bg-black  p-3 rounded-full pl-6 pr-4 text-white hover:bg-black/10 trasitions-all duration-300 hover:gap-3 active:scale-105 dir ">
+					Explore
+					<span className="text-3xl  ">
+						<GoArrowRight />
+					</span>
+				</button>
+			</div>
 
-      <div className="flex justify-center mt-7  ">
-        <button className="flex items-center gap-2 border bg-black  p-3 rounded-full pl-6 pr-4 text-white hover:bg-black/10 trasitions-all duration-300 hover:gap-3 active:scale-105 dir ">
-          Explore
-          <span className="text-3xl  ">
-            <GoArrowRight />
-          </span>
-        </button>
-      </div>
-
-      <div className="flex justify-center">
-        <div className="mt-[35px]  lg:w-[70%]  md:w-[80%] ">
-          <Swiper
-            effect={"coverflow"}
-            grabCursor={true}
-            centeredSlides={true}
-            slidesPerView={"auto"}
-            coverflowEffect={{
-              rotate: 50,
-              stretch: 0,
-              depth: 100,
-              modifier: 1,
-              slideShadows: true,
-            }}
-            pagination={true}
-            modules={[EffectCoverflow, Pagination, Navigation]}
-            className="mySwiper "
-          >
-            <SwiperSlide>
-              <img
-                src="/bannerImage.webp"
-                alt="Banner Image"
-                width="full"
-                height="full"
-              />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img
-                src="/bannerImage.webp"
-                alt="Banner Image"
-                width="full"
-                height="full"
-              />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img
-                src="/bannerImage.webp"
-                alt="Banner Image"
-                width="full"
-                height="full"
-              />
-            </SwiperSlide>
-          </Swiper>
-        </div>
-      </div>
-    </div>
-  );
+			<div className="flex justify-center">
+				<div className="mt-[35px]  lg:w-[70%]  md:w-[80%] ">
+					<Swiper
+						effect={"coverflow"}
+						grabCursor={true}
+						centeredSlides={true}
+						slidesPerView={"auto"}
+						coverflowEffect={{
+							rotate: 50,
+							stretch: 0,
+							depth: 100,
+							modifier: 1,
+							slideShadows: true,
+						}}
+						pagination={true}
+						modules={[EffectCoverflow, Pagination, Navigation]}
+						className="mySwiper "
+					>
+						<SwiperSlide>
+							<img
+								src="/bannerImage.webp"
+								alt="Banner Image"
+								width="full"
+								height="full"
+							/>
+						</SwiperSlide>
+						<SwiperSlide>
+							<img
+								src="/bannerImage.webp"
+								alt="Banner Image"
+								width="full"
+								height="full"
+							/>
+						</SwiperSlide>
+						<SwiperSlide>
+							<img
+								src="/bannerImage.webp"
+								alt="Banner Image"
+								width="full"
+								height="full"
+							/>
+						</SwiperSlide>
+					</Swiper>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default HeroBanner;
